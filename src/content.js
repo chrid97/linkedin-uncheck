0b12e08666f5041ffd80f4d0a8c508815e3b4d12
chrid97 <cgoncal12@gmail.com>
2024-06-17 19:50:31 -0400

    Remove unnecessary files

--- src/content.js
+++ src/content.js
@@ -1,12 +1,13 @@
-const observer = new MutationObserver((mutationList) => {
-    for (let mutation of mutationList) {
-        for (let node of mutation.addedNodes) {
-            if (node.id === "follow-company-checkbox") {
-                node.checked = false;
-            }
-        }
-    }
-});
+// const observer = new MutationObserver((mutationList) => {
+//     for (let mutation of mutationList) {
+//         for (let node of mutation.addedNodes) {
+//             if (node.id === "follow-company-checkbox") {
+//                 node.checked = false;
+//             }
+//         }
+//     }
+// });
 
-observer.observe(document.body, { childList: true, subtree: true });
+console.log("yo")
 
+// observer.observe(document.body, { childList: true, subtree: true });
